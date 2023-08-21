ed2bdfa75611e064611b86677e6e2d239598593d
tfutada <tafutada@gmail.com>
2023-08-21 14:08:09 +0900

    jeopardy

--- jeopardy.py
+++ jeopardy.py
@@ -35,38 +35,41 @@ class_obj = {
     # If set to "none" you must always provide vectors yourself. Could be any other "text2vec-*" also.
     "moduleConfig": {
         "text2vec-openai": {},
-        "generative-openai": {}  # Ensure the `generative-openai` module is used for generative queries
+        "generative-openai": {
+            "model": "gpt-3.5-turbo"
+        }  # Ensure the `generative-openai` module is used for generative queries
     }
 }
 
-client.schema.create_class(class_obj)
+# client.schema.create_class(class_obj)
 
 jeopardy_url = 'https://raw.githubusercontent.com/databyjp/wv_demo_uploader/main/weaviate_datasets/data/jeopardy_1k.json'
 
-resp = requests.get(jeopardy_url)
-data = json.loads(resp.text)  # Load data
-
-with client.batch() as batch:  # Initialize a batch process
-    for i, d in enumerate(data):  # Batch import data
-        print(f"importing question: {i+1}")
-        properties = {
-            "answer": d["Answer"],
-            "question": d["Question"],
-        }
-        batch.add_data_object(
-            data_object=properties,
-            class_name="JeopardyQuestion"
-        )
+# resp = requests.get(jeopardy_url)
+# data = json.loads(resp.text)  # Load data
+#
+# with client.batch() as batch:  # Initialize a batch process
+#     for i, d in enumerate(data):  # Batch import data
+#         print(f"importing question: {i+1}")
+#         properties = {
+#             "answer": d["Answer"],
+#             "question": d["Question"],
+#         }
+#         batch.add_data_object(
+#             data_object=properties,
+#             class_name="JeopardyQuestion"
+#         )
 
 #
 # === Querying ===
 #
 
-# response = (
+# response1 = (
 #     client.query
 #     .get("JeopardyQuestion", ["question", "answer"])
+#     .with_additional("vector")
 #     .with_near_text({
-#         "concepts": ["animals in movies"]
+#         "concepts": ["animals in movies commented by US presidents"]
 #     })
 #     .with_limit(2)
 #     .with_additional(["distance"])
@@ -84,7 +87,19 @@ with client.batch() as batch:  # Initialize a batch process
 #     .do()
 # )
 
-# print(json.dumps(response1, indent=2))
+generate_prompt = "Convert the following into a question for twitter. Include emojis for fun, but do not include the answer: {question}."
+
+response = (
+  client.query
+  .get("JeopardyQuestion", ["question"])
+  .with_generate(single_prompt=generate_prompt)
+  .with_near_text({
+    "concepts": ["World history"]
+  })
+  .with_limit(2)
+).do()
+
+print(json.dumps(response, indent=2))
 
 
 if __name__ == '__main__':
